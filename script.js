41fc6ef0967f434eb1b106f34dbbcd32762b5d7a
david.parra.cruz <69942426+parrrra@users.noreply.github.com>
2025-12-23 10:33:20 +0100

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const botonInicio = document.getElementById("botonInicio");
 const botonFinal = document.getElementById("botonFinal");
 const contenido = document.getElementById("contenido");
 const final = document.getElementById("final");
+const modem = document.getElementById("modem");
 
 const musica = document.getElementById("musica");
 const sonidoBoton = document.getElementById("sonidoBoton");
@@ -18,3 +19,33 @@ botonFinal.addEventListener("click", () => {
   contenido.style.display = "none";
   final.style.display = "block";
 });
+
+const loading = document.getElementById("loading");
+
+window.addEventListener("load", () => {
+  modem.volume = 0.6;
+  modem.play();
+  // Intentamos reproducir (funciona en la mayoría de navegadores)
+  modem.play().catch(() => {
+    console.log("El navegador bloqueó el autoplay del módem 😢");
+  });
+
+  setTimeout(() => {
+    loading.style.display = "none";
+    modem.pause();
+    modem.currentTime = 0;
+  }, 15000);
+});
+
+const textos = [
+  "Cargando tejas...",
+  "Buscando mantas...",
+  "Encendiendo estufa de butano...",
+  "Espantando arañas...",
+  "Aplicando humedad...",
+];
+
+setInterval(() => {
+  document.querySelector("#loading p").innerText =
+    textos[Math.floor(Math.random() * textos.length)];
+}, 800);
